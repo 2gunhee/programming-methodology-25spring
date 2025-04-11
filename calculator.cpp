9336b7568d553182429f8b719c1e7af7fd483c3e
gunhee <gunhee051211@snu.ac.kr>
2025-04-11 17:26:53 +0900

    test complete

--- calculator.cpp
+++ calculator.cpp
@@ -3,16 +3,72 @@
 
 int Calculator::add(int a, int b) {
     // TODO
+    if (a >= INT_MAX || b >= INT_MAX)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else if (a <= INT_MIN || b <= INT_MIN)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else
+    {
+        return a+b;
+    }
 }
 
 int Calculator::sub(int a, int b) {
     // TODO
+    if (a >= INT_MAX || b >= INT_MAX)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else if (a <= INT_MIN || b <= INT_MIN)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else
+    {
+        return a-b;
+    }
 }
 
 int Calculator::mul(int a, int b) {
     // TODO
+    if (b != 0)
+    {
+        if (a >= INT_MAX / b || b >= INT_MAX)
+        {
+            throw std::overflow_error("overflow error");
+        }
+        else if (a <= INT_MIN / b || b <= INT_MIN)
+        {
+            throw std::overflow_error("overflow error");
+        }
+        else
+        {
+            return a*b;
+        }
+    }
+    return 0;
 }
 
 int Calculator::div(int a, int b) {
     // TODO
+    if (b == 0)
+    {
+        throw std::invalid_argument("Division by zero");
+    }
+    else if (a >= INT_MAX || b >= INT_MAX)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else if (a <= INT_MIN || b <= INT_MIN)
+    {
+        throw std::overflow_error("overflow error");
+    }
+    else
+    {
+        return a/b;
+    }
 }
